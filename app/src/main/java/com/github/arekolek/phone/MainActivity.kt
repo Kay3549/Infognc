a4f9aae15b524f3a73194154e29abbea3eec7aad
infognc <dlwlaks552@naver.com>
2021-02-01 18:13:49 +0900

    app화면

--- app/src/main/java/com/github/arekolek/phone/MainActivity.kt
+++ app/src/main/java/com/github/arekolek/phone/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.github.arekolek.phone
 
-import android.os.*
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.StrictMode
 import android.util.Log
-import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import androidx.appcompat.app.AppCompatActivity
-import java.sql.*
-import java.util.*
-import kotlin.collections.ArrayList
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.sql.Statement
 
-
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity()  {
     private val ip = "192.168.1.206"
     private val port = "1433"
     private val Classes = "net.sourceforge.jtds.jdbc.Driver"
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private val password = ".Digital"
     private val url = "jdbc:jtds:sqlserver://$ip:$port/$database"
     private var connection: Connection? = null
+    private var data = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,25 +41,36 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
             Log.d("sqlDB", "ERROR")
         }
-
         sqlDB()
+        var list = findViewById<ListView>(R.id.listview)
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        list.setAdapter(adapter)
 
+        adapter.addAll(data)
+        adapter.notifyDataSetChanged()
+
+        Log.d("sqlCall","적재성공")
+        
+        list.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            var intent = Intent(this, MainActivity_click::class.java)
+            intent.putExtra("DB", data[position])
+            startActivity(intent)
+        }
     }
-
-    private fun sqlDB(){
-        var data = ArrayList<String>()
+    fun sqlDB(){
+        data = ArrayList<String>()
         if (connection != null) {
             var statement: Statement? = null
             try {
                 statement = connection!!.createStatement()
                 val sql =
-                    "select db.custNum,db.counStep,db.alocdate,info.custName from customer_db as db  left outer join customer_info as info \n" +
-                            "on db.custnum = info.custnum\n where agentNum = '1'"
+                        "select db.custNum,db.counStep,db.alocdate,info.custName from customer_db as db  left outer join customer_info as info \n" +
+                                "on db.custnum = info.custnum\n where agentNum = '1'"
                 val resultSet = statement.executeQuery(sql) // DB
 
 
                 while (resultSet.next()) {
-                    var str = resultSet.getString(1) + " " + resultSet.getString(4) + " " + resultSet.getString(2)+ " " + resultSet.getString(3)
+                    var str = resultSet.getString(1) + " | " + resultSet.getString(4) + " | " + resultSet.getString(2)+ " | " + resultSet.getString(3)
                     data.add(str)
                 }
             } catch (e: SQLException) {
@@ -67,20 +79,13 @@ class MainActivity : AppCompatActivity() {
         } else {
             Log.d("sqlDB", "Connection is null")
         }
-
-        var list = findViewById<ListView>(R.id.listview)
-
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
-        list.setAdapter(adapter)
-
-        adapter.addAll(data)
-        adapter.notifyDataSetChanged()
-
-        list.setOnItemClickListener(AdapterView.OnItemClickListener() {
-                adapterView: AdapterView<*>, view: View, position: Int, l: Long ->
-            fun OnItemClick(){
-                var choise:String = adapterView.getItemAtPosition(position) as String
-            }
-        })
     }
+
+    /*override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var list = findViewById<ListView>(R.id.listview)
+        var intent = Intent(this, MainActivity_clicklist::class.java)
+        intent.putExtra("custNum", data[position])
+        startActivity(intent)
+    }*/
+
 }
