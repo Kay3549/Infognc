8d92f0b27fa776e494eb3a39427a54d4751293f3
infognc <dlwlaks552@naver.com>
2021-02-19 15:57:05 +0900

    UI_완성

---
+++ app/src/main/java/com/github/arekolek/phone/Viewer1.kt
@@ -0,0 +1,59 @@
+package com.github.arekolek.phone
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+
+class Viewer1():BaseAdapter(){ //private val items: MutableList<Itemlist>
+    var listViewItemList = ArrayList<Itemlist>()
+
+    override fun getCount(): Int {
+        return listViewItemList.size
+    }
+
+    override fun getItem(position:Int): Itemlist {
+        return listViewItemList.get(position)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, view: View?, parent:ViewGroup?):View{
+        var mcontext: Context? = parent?.context
+
+        var inflater = mcontext?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var convertView = inflater.inflate(R.layout.listview_item1,parent,false)
+
+
+        val item: Itemlist = listViewItemList.get(position)
+        var numtextview = convertView.findViewById<TextView>(R.id.num)
+        var startcalltextview = convertView.findViewById<TextView>(R.id.startcall)
+        var gogeaktextview = convertView.findViewById<TextView>(R.id.gogaek)
+        var counttextview = convertView.findViewById<TextView>(R.id.count)
+
+
+        numtextview.setText(item.getNum())
+        startcalltextview.setText(item.getStartcall())
+        gogeaktextview.setText(item.getGogeak())
+        counttextview.setText(item.getCount())
+
+        return convertView
+    }
+
+    fun addItem(num: String, startcall: String, gogeak: String, count: String){
+        var item:Itemlist? = Itemlist()
+        item?.setNum(num)
+        item?.setStartcall(startcall)
+        item?.setGogeak(gogeak)
+        item?.setCount(count)
+
+        if (item != null) {
+            listViewItemList.add(item)
+        }
+    }
+}
